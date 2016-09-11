50ce0792a39879c822558a0f13ded0345fc5713d
xiejiangchu <xiejiangchu@163.com>
2016-09-11 11:46:20 +0800

    1.log4j2 配置 2.属性@Value取值

--- src/main/java/com/xie/mj/service/UserService.java
+++ src/main/java/com/xie/mj/service/UserService.java
@@ -3,6 +3,7 @@ package com.xie.mj.service;
 import com.xie.mj.mapper.UserMapper;
 import com.xie.mj.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,7 +15,14 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Value("${test}")
+    private String hello;
+
     public User getFirstUser() {
         return userMapper.selectByPrimaryKey(1);
+    }
+
+    public String getEnv() {
+        return hello;
     }
 }
